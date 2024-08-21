273a1af5dff226a472803abc26433544603eea96
parkyooni <elmi1003@naver.com>
2024-08-21 14:33:37 +0900

    Refactor: Code refactoring ProjectStore

--- src/renderer/store/projectStore.js
+++ src/renderer/store/projectStore.js
@@ -1,5 +1,20 @@
 import { create } from "zustand";
 
+const checkProjectStarterValidity = state => {
+  return !!state.path && !!state.selectedPackageManager && !!state.projectName;
+};
+const updateState = (set, state, field, value) => {
+  const newState = {
+    ...state,
+    [field]: value
+  };
+
+  return {
+    ...newState,
+    isProjectStarterValid: checkProjectStarterValidity(newState)
+  };
+};
+
 const useProjectStore = create(set => ({
   path: "",
   selectedPackageManager: "",
@@ -8,10 +23,8 @@ const useProjectStore = create(set => ({
   projects: [],
   selectedSettingOption: "userDefined",
   isUserDefinedSetting: true,
-
   isProjectStarterValid: false,
   isDependenciesSelected: false,
-
   searchQuery: "",
   packageItems: [],
   selectedPackageItem: null,
@@ -24,53 +37,19 @@ const useProjectStore = create(set => ({
       isUserDefinedSetting: option === "userDefined"
     })),
 
-  setPath: path => {
-    set(state => {
-      const isProjectStarterValid =
-        !!path && !!state.selectedPackageManager && !!state.projectName;
-      return {
-        path,
-        isProjectStarterValid
-      };
-    });
-  },
-
-  setSelectedPackageManager: selectedPackageManager => {
-    set(state => {
-      const isProjectStarterValid =
-        !!state.path && !!selectedPackageManager && !!state.projectName;
-      return {
-        selectedPackageManager,
-        isProjectStarterValid
-      };
-    });
-  },
-
-  setProjectName: projectName => {
-    set(state => {
-      const isProjectStarterValid =
-        !!state.path && !!state.selectedPackageManager && !!projectName;
-      return {
-        projectName,
-        isProjectStarterValid
-      };
-    });
-  },
+  setPath: path => set(state => updateState(set, state, "path", path)),
+  setSelectedPackageManager: selectedPackageManager =>
+    set(state => updateState(set, state, "selectedPackageManager", selectedPackageManager)),
+  setProjectName: projectName => set(state => updateState(set, state, "projectName", projectName)),
 
   setFiles: files => set({ files }),
-
-  setUserDefinedSetting: isUserDefined =>
-    set({ isUserDefinedSetting: isUserDefined }),
-
-  setDependenciesSelected: isSelected =>
-    set({ isDependenciesSelected: isSelected }),
-
+  setUserDefinedSetting: isUserDefined => set({ isUserDefinedSetting: isUserDefined }),
+  setDependenciesSelected: isSelected => set({ isDependenciesSelected: isSelected }),
   setSearchQuery: query => set({ searchQuery: query }),
   setPackageItems: items => set({ packageItems: items }),
   setSelectedPackageItem: item => set({ selectedPackageItem: item }),
   setIsDropdownVisible: isVisible => set({ isDropdownVisible: isVisible }),
   setIsEnterPressed: isPressed => set({ isEnterPressed: isPressed }),
-
   setProjects: projects => set({ projects }),
 
   checkProjectPath: path => {
