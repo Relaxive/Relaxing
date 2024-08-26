537f52aa816ceabbf536bfa2991fce6a900e4d3c
parkyooni <elmi1003@naver.com>
2024-08-27 05:41:05 +0900

    Chore: Add conditional logic in ErrorModal for missing project list paths

--- src/renderer/components/Modal/ErrorModal.jsx
+++ src/renderer/components/Modal/ErrorModal.jsx
@@ -1,13 +1,19 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import ButtonBox from "@components/common/ButtonBox";
 import { ModalBackground, ModalContainer } from "@public/style/Modal.styles";
 
 const ErrorModal = ({ message, onClose }) => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleClose = () => {
     onClose();
-    navigate(-1);
+
+    if (location.pathname !== "/project/project-list") {
+      navigate("/project/project-list");
+    } else {
+      navigate(-1);
+    }
   };
 
   return (
